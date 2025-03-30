22ec8e9302cbfd68604a646d2b15996e7796f88f
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2025-03-30 20:47:54 +0100

    finished portfolio started contact form

--- libs/js/script.js
+++ libs/js/script.js
@@ -45,34 +45,112 @@ const animateSkillSquare = () => {
 
 animateSkillSquare();
 
-$('.switch-1').on('click', function () {
-  $(this).closest('.group').attr('aria-disabled', 'true');
-  $(this).closest('.controller-div').attr('aria-expanded', 'false');
-});
-
-$('.switch-2').on('click', function () {
-  $(this).closest('.group').attr('aria-disabled', 'false');
-  $(this).closest('.controller-div').attr('aria-expanded', 'true');
-});
-
 let index = 0;
-const imgWidth = $('.slider-track').width() + 64;
 
-function updateSlider() {
-  $('.slider-track').css('transform', `translateX(${-index * imgWidth}px)`);
+function updateSlider(sliderTrack) {
+  const imgWidth = sliderTrack.width() + 64;
+  sliderTrack.css('transform', `translateX(${-index * imgWidth}px)`);
 }
 
+$('.map-switch-1').on('click', function () {
+  $(this).closest('.group').attr('aria-disabled', 'true');
+  $(this).closest('.controller-div').attr('aria-expanded', 'false');
+  $('#map-overview').attr('aria-expanded', 'false');
+
+  index = 0;
+
+  $('.slider-track').each(function () {
+    const sliderTrack = $(this);
+
+    sliderTrack.css('transform', `translateX(0px)`);
+  });
+});
+
+$('.map-switch-2').on('click', function () {
+  $(this).closest('.group').attr('aria-disabled', 'false');
+  $(this).closest('.controller-div').attr('aria-expanded', 'true');
+  $('#map-overview').attr('aria-expanded', 'true');
+
+  index = 0;
+
+  $('.slider-track').each(function () {
+    const sliderTrack = $(this);
+
+    sliderTrack.css('transform', `translateX(0px)`);
+  });
+});
+
+$('#overview-switch').on('click', function () {
+  const isOverviewExpanded =
+    $('#map-overview').attr('aria-expanded') === 'true';
+
+  $('#map-overview').attr(
+    'aria-expanded',
+    isOverviewExpanded ? 'false' : 'true'
+  );
+
+  $('.map-switch-1')
+    .closest('.group')
+    .attr('aria-disabled', isOverviewExpanded ? 'true' : 'false');
+
+  $('.map-switch-1')
+    .closest('.controller-div')
+    .attr('aria-expanded', isOverviewExpanded ? 'false' : 'true');
+
+  index = 0;
+
+  $('.slider-track').each(function () {
+    $(this).css('transform', `translateX(0px)`);
+  });
+});
+
+$('.exit-overview').on('click', function () {
+  $(this).closest('.fixed').attr('aria-disabled', 'true');
+  $('body').css('overflow', 'auto');
+
+  $('.slider-track').each(function () {
+    $(this).css('transform', `translateX(0px)`);
+  });
+});
+
+$('#map-overview-button').on('click', function () {
+  $('#map-overview').attr('aria-disabled', 'false');
+  $('body').css('overflow', 'hidden');
+});
+
+$('#taskx-overview-button').on('click', function () {
+  $('#taskx-overview').attr('aria-disabled', 'false');
+  $('body').css('overflow', 'hidden');
+});
+
+$('#whatsong-overview-button').on('click', function () {
+  $('#whatsong-overview').attr('aria-disabled', 'false');
+  $('body').css('overflow', 'hidden');
+});
+
+$('#hackernews-overview-button').on('click', function () {
+  $('#hackernews-overview').attr('aria-disabled', 'false');
+  $('body').css('overflow', 'hidden');
+});
+
 $('.next-photo').on('click', function () {
-  if (index < $('.slider-track img:visible').length - 1) {
+  let sliderTrack = $(this)
+    .closest('.slider-container')
+    .find('.slider-track:visible');
+  const visibleImages = sliderTrack.find('img:visible');
+
+  if (index < visibleImages.length - 1) {
     index++;
-    updateSlider();
+    updateSlider(sliderTrack);
   }
 });
 
 $('.prev-photo').on('click', function () {
+  let sliderTrack = $(this).closest('.slider-container').find('.slider-track');
+
   if (index > 0) {
     index--;
-    updateSlider();
+    updateSlider(sliderTrack);
   }
 });
 
@@ -89,4 +167,12 @@ $('.next-photo').on('mouseenter', function () {
 
 $('.next-photo').on('mouseleave', function () {
   $(this).closest('div').css('--right-grad', 'rgba(0,0,0,.5)');
+});
+
+$('contact-button').on('click', function () {
+  $.ajax({
+    url: '/api/mailer',
+    method: 'POST',
+    dataType: 'json',
+  });
 });
